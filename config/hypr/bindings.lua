62a3a8e640222e7f20f50886fb317542aac213b7
Daniel Miguel Tejedor <danielmigueltejedor@icloud.com>
2026-05-19 00:26:05 +0200

    Fix command bar toggle close behavior

--- config/hypr/bindings.lua
+++ config/hypr/bindings.lua
@@ -49,4 +49,7 @@ o.bind("CTRL + ALT + 2", "Type @", "wtype @")
 
 -- Nodalix Command Bar
 hl.unbind("SUPER + SPACE")
-o.bind("SUPER + SPACE", "Nodalix Command Bar", { launch = "nodalix-command-bar" })
+
+-- Nodalix Command Bar
+hl.unbind("SUPER + SPACE")
+o.bind("SUPER + SPACE", "Nodalix Command Bar", { launch = "nodalix-command-bar-toggle" })
