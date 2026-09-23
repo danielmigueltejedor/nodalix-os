3147d6886ec20324a44db05211cb16671915e038
Daniel Miguel <danielmigueltejedor@icloud.com>
2026-09-24 01:19:20 +0200

    fix: integrate Nodalix Apps with application updates

--- shell/scripts/update/nodalix-update-apps.sh
+++ shell/scripts/update/nodalix-update-apps.sh
@@ -4,6 +4,10 @@ set -eu
 if command -v yay >/dev/null 2>&1; then
     yay -Sua --noconfirm
 fi
+if command -v nodalix-apps >/dev/null 2>&1; then
+    nodalix-apps update --all
+fi
+
 if command -v flatpak >/dev/null 2>&1; then
     flatpak update -y --noninteractive
 fi
